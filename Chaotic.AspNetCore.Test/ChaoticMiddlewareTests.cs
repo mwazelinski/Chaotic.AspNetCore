3b677de18e113e24e5c0863c9af26dbffc0ef430
Marcin <mwazelinski@gmail.com>
2016-08-23 23:21:26 +0200

    Initial projects structure with tests

---
+++ Chaotic.AspNetCore.Test/ChaoticMiddlewareTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Chaotic.AspNetCore.Test
+{
+    public class ChaoticMiddlewareTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
